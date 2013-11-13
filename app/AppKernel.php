c3e9da82abe17f4825af24f0c77ad7e01dd17c4d
soyel <contact@soyel.fr>
2013-11-13 10:36:23 +0100

    Working Sonata Project

---
+++ app/AppKernel.php
@@ -0,0 +1,55 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+			new Sonata\MarkItUpBundle\SonataMarkItUpBundle(),
+			new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+			new Sonata\NewsBundle\SonataNewsBundle(),
+			new Sonata\UserBundle\SonataUserBundle(),
+			new Sonata\MediaBundle\SonataMediaBundle(),
+			new Sonata\AdminBundle\SonataAdminBundle(),
+			new Sonata\IntlBundle\SonataIntlBundle(),
+			new Sonata\BlockBundle\SonataBlockBundle(),
+			new Sonata\jQueryBundle\SonatajQueryBundle(),
+			new Sonata\FormatterBundle\SonataFormatterBundle(),
+			new Sonata\ClassificationBundle\SonataClassificationBundle(),
+			new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+			new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
+			new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+			new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+			new Application\Sonata\NewsBundle\ApplicationSonataNewsBundle(),
+			new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
+			new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+			new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+			new Application\Sonata\ClassificationBundle\ApplicationSonataClassificationBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
